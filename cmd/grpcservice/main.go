b81baae00a62a7d83fafacb31a7d611600b5c986
Nuru <ceruks@eteb.me>
2025-10-03 20:19:13 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/grpcservice/main.go
@@ -0,0 +1,23 @@
+// cmd/grpcservice/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "grpcservice/internal/grpcservice"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := grpcservice.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
